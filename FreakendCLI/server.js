7d1348f7583f8c8eeb1442537fe8db3cd5057ce0
Himeshchanchal Bhattarai <144266989+Himesh-Bhattarai@users.noreply.github.com>
2025-06-24 22:11:53 +0545

    license added

--- FreakendCLI/server.js
+++ FreakendCLI/server.js
@@ -1,10 +1,10 @@
 
+dotenv.config();
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 
-dotenv.config();
 const app = express();
 app.use(express.json());
 
